cf429b22ba20d75a96a9b56d4815b561809d1d97
Harshsh00 <businessorg76@gmail.com>
2024-04-04 11:33:06 +0530

    first commmit

---
+++ index.js
@@ -0,0 +1,13 @@
+
+import chalk from "chalk"
+
+//color provide
+console.log(chalk.blue('have a fuck day')) 
+console.log(chalk.magenta('have a fuck day'))
+console.log(chalk.white('congrats to get a fuck'))
+
+// give color using hex value
+
+const warning = chalk.hex('#FFA500');
+
+console.log(warning('you fucked up'))
